e1fcb51b3c326acad3cbd1bd410816a68fb7ace8
kevin940726 <kevin830726@gmail.com>
2016-06-26 19:00:58 +0800

    <refactor> dymanic require

--- src/index.js
+++ src/index.js
@@ -1,35 +1,25 @@
-import $ from 'jquery';
+/* eslint-disable global-require */
 import page from 'page';
-import Home from './views/home.ejs';
-import About from './views/about.ejs';
-import NotFound from './views/404.ejs';
+// import Home from './views/home.ejs';
+import App from './components/App';
+// import About from './views/about.ejs';
+// import NotFound from './views/404.ejs';
+import React from 'react';
+import { render } from 'react-dom';
+import './style.global.css';
 
-const routingCallback = (view, data = {}) => () => {
-	document.getElementById('view').innerHTML = view(data);
+const routingCallback = (view, data = {}) => (ctx, next) => {
+	require.ensure([], require => {
+		const req = require.context('./views', true, /\.ejs$/);
+		const module = req(view);
+		document.getElementById('view').innerHTML = module(data);
+		next(ctx);
+	});
 };
 
-page('/', ctx => {
-	if (!ctx.state.todos) {
-		ctx.state.todos = [
-			'setup enviroment',
-			'write code',
-			'take a break',
-			'back to work',
-		];
-
-		$('.todos .todo .doneBtn').click(function (e) {
-			e.preventDefault();
-			ctx.state.todos.splice($(this).parent().index(), 1);
-			console.log(ctx.state.todos, $(this).parent().index());
-
-			page('/');
-		});
-	} else {
-		document.getElementById('view').innerHTML = Home({
-			todos: ctx.state.todos,
-		});
-	}
+page('/', routingCallback('./home.ejs'), () => {
+	render(<App />, document.getElementById('root'));
 });
-page('/about', routingCallback(About));
-page('*', routingCallback(NotFound));
+page('/about', routingCallback('./about.ejs'));
+page('*', routingCallback('./404.ejs'));
 page.start();
--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -1,15 +1,19 @@
+/* eslint-disable */
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	devtool: 'source-map',
-	entry: [
-		'./src/index',
-		'./src/entry'
-	],
+	entry: {
+		app: './src/index',
+		vendor: ['page', 'react', 'react-dom']
+	},
 	output: {
 		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js',
+		filename: '[name].[chunkhash].js',
+		chunkFilename: "[name].[chunkhash].chunk.js",
 		publicPath: ''
 	},
 	module: {
@@ -21,12 +25,12 @@ module.exports = {
 		    },
             {
                 test: /\.global\.css$/,
-                loader: 'style-loader!css-loader?sourceMap!postcss-loader',
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap!postcss-loader'),
                 include: path.join(__dirname, 'src')
             },
             {
     			test: /^((?!\.global).)*\.css$/,
-                loader: 'style-loader!css-loader?modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader',
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'),
                 include: path.join(__dirname, 'src')
 		    },
 			{
@@ -39,8 +43,8 @@ module.exports = {
 		    },
 			{
 				test: /\.ejs$/,
-				loader: 'file?name=[name].html!ejs-html!html-minifier',
-				include: path.join(__dirname, 'src')
+				loader: 'ejs-compiled',
+				include: path.join(__dirname, 'src/views')
 			},
         ]
 	},
@@ -60,6 +64,8 @@ module.exports = {
 	      }
 	    }),
 
+		new webpack.optimize.CommonsChunkPlugin('vendor', '[name].[chunkhash].js'),
+
 	    // OccurrenceOrderPlugin is needed for long-term caching to work properly.
 	    // See http://mxs.is/googmv
 	    new webpack.optimize.OccurrenceOrderPlugin(true),
@@ -73,5 +79,25 @@ module.exports = {
 	        warnings: false, // ...but do not show warnings in the console (there is a lot of them)
 	      },
 	    }),
+
+		new HtmlWebpackPlugin({
+			template: './src/index.ejs',
+			filename: 'index.html',
+			minify: {
+		        removeComments: true,
+		        collapseWhitespace: true,
+		        removeRedundantAttributes: true,
+		        useShortDoctype: true,
+		        removeEmptyAttributes: true,
+		        removeStyleLinkTypeAttributes: true,
+		        keepClosingSlash: true,
+		        minifyJS: true,
+		        minifyCSS: true,
+		        minifyURLs: true,
+		    },
+		}),
+
+		// Extract the CSS into a seperate file
+	    new ExtractTextPlugin('[name].[contenthash].css'),
 	],
 };
