03e9c8c9eede9dfc80c5480810e5cda9f24fe9fa
kevin940726 <kevin830726@gmail.com>
2016-06-29 00:46:05 +0800

    <feat> fake api server with json-server

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ var config = require('./webpack.config');
 var pacakge = require('./package.json');
 var ip = require('ip');
 var argv = require('yargs')
-    .default('p', pacakge.config.port || 8000)
+    .default('p', pacakge.config.PORT || 8000)
+    .default('d', pacakge.config.DB_PORT || 8001)
     .argv;
+var jsonServer = require('json-server');
 
 var PORT = argv.p;
+var DB_PORT = argv.d;
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
@@ -24,4 +27,15 @@ new WebpackDevServer(webpack(config), {
 
   console.log(`Server started at http://localhost:${PORT}/`);
   console.log(`Tunnel started at http://${ip.address()}:${PORT}/`);
+
+  jsonServer.create()
+      .use(jsonServer.defaults())
+      .use(jsonServer.router('db.json'))
+      .listen(DB_PORT, function(err) {
+          if (err) {
+              return console.log(err);
+          }
+
+          console.log(`Fake api server opened at port ${DB_PORT}`);
+      })
 });
