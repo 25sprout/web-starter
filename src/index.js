d1df5faefbbac0457e505cf2417efb75704d43f5
kevin940726 <kevin830726@gmail.com>
2016-06-29 11:31:43 +0800

    <feat> add normalize.css

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import createLogger from 'redux-logger';
 import createReducer from './reducers';
 import Routes from './routes';
 import { install } from 'offline-plugin/runtime';
+import 'normalize-css';
 import './style.global.css';
 
 const logger = createLogger();
