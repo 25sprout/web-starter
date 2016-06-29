cc4b263dd4a4ceee8666d3ab916d60a39a30e5ee
kevin940726 <kevin830726@gmail.com>
2016-06-29 15:10:41 +0800

    <refactor> remove redux-logger

--- src/index.js
+++ src/index.js
@@ -4,39 +4,23 @@ import { AppContainer } from 'react-hot-loader';
 import { createStore, compose, applyMiddleware } from 'redux';
 import { browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
-import createLogger from 'redux-logger';
-// import * as reducers from './reducers';
 import createReducer from './reducers';
 import Routes from './routes';
 import { install } from 'offline-plugin/runtime';
 import 'sanitize.css/sanitize.css';
 import './style.global.css';
 
-const logger = createLogger();
-
 const store = createStore(
 	createReducer(),
 	{},
 	compose(
-		applyMiddleware(logger),
+		applyMiddleware(),
 		window.devToolsExtension ? window.devToolsExtension() : f => f
 	)
 );
 
 // async reducers registry
 store.asyncReducers = {};
-
-// Make reducers hot reloadable, see http://mxs.is/googmo
-/* eslint-disable global-require */
-if (module.hot) {
-	module.hot.accept('./reducers', () => {
-		const createReducers = require('./reducers').default;
-		const nextReducers = createReducers(store.asyncReducers);
-
-		store.replaceReducer(nextReducers);
-	});
-}
-/* eslint-disable global-require */
 
 const history = syncHistoryWithStore(browserHistory, store);
 
@@ -47,6 +31,14 @@ render(
 
 /* eslint-disable global-require */
 if (module.hot) {
+	// Make reducers hot reloadable, see http://mxs.is/googmo
+	module.hot.accept('./reducers', () => {
+		const createReducers = require('./reducers').default;
+		const nextReducers = createReducers(store.asyncReducers);
+
+		store.replaceReducer(nextReducers);
+	});
+
 	module.hot.accept('./routes', () => {
 		render(
 			<AppContainer
